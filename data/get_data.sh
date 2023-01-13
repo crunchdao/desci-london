61798f4f445619509d925d1e31027f31a4fda3c0
matteoettam09 <matteomanzi09@gmail.com>
2023-01-13 18:50:44 +0100

    fix: ipfs data on crunchdao gateway

--- data/get_data.sh
+++
@@ -1,3 +0,0 @@
-ipfs cat /ipfs/QmTBdkekrUFAFtTv9LwGrsiMTriMztqQyofiEdtYsswBBm > ./data/target.parquet
-ipfs cat /ipfs/QmPuAmU4cRWKmJZcQ6SBBhGRiCvq3fmQ64ujPHEJFrE1Mw > ./data/features.parquet
-
---
+++ get_data.sh
@@ -0,0 +1,2 @@
+wget -O ./data/features.parquet "https://gateway.crunchdao.com/ipfs/QmPuAmU4cRWKmJZcQ6SBBhGRiCvq3fmQ64ujPHEJFrE1Mw"
+wget -O ./data/target.parquet "https://gateway.crunchdao.com/ipfs/QmTBdkekrUFAFtTv9LwGrsiMTriMztqQyofiEdtYsswBBm"
